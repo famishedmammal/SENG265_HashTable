719b266591c8963a8225d3e16355609caa7495c1
Harrison Walters <famishedmammal@gmail.com>
2016-02-01 22:19:07 -0800

    Update string_set.cpp

--- string_set.cpp
+++ string_set.cpp
@@ -1,3 +1,10 @@
+/*------------------------------
+* String_set.h
+* Description: A program that hashes an input string to a table.
+*
+* By: Harrison Walters
+-------------------------------*/
+
 #include <iostream>
 #include <stdlib.h>
 #include <stdio.h>
