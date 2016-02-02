994c9b9e6fc64ec15601fb013e1b1716cf0c995e
Harrison Walters <famishedmammal@gmail.com>
2016-02-01 22:17:24 -0800

    Create string_set.h

---
+++ string_set.h
@@ -0,0 +1,34 @@
+using namespace std;
+
+class string_set {
+
+public:
+	string_set();
+	void add(const char *s);
+	void remove(const char *s);
+	int contains(const char *s);
+	void reset();
+	const char *next();
+	~string_set();
+
+private:
+
+	int hash_function(const char *s);
+
+	enum {HASH_TABLE_SIZE = 100};
+
+	class node {
+		public:
+		char *s;
+		node *next;
+	};
+
+	node *hash_table[HASH_TABLE_SIZE];
+
+	int iterator_index; // index in hash_table
+	node *iterator_node; // node in hash_table[iterator_index];
+};
+
+class duplicate_exception { };
+class memory_exception { };
+class not_found_exception { };
